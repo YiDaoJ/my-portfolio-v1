94a91106141979c1dea97d63cf53624f8bae3559
YiDaoJ <jcxsusan@gmail.com>
2022-02-22 21:15:27 +0100

    upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#pagepiling').pagepiling({
+        verticalCentered: false,
+        css3: false,
+        navigation: {
+                  'position': 'right',
+                  'tooltips': ['Home', 'About Me', 'Project &amp; Demos ', 'Skills', 'Contact Me'], 
+        },
+
+    });
+});
